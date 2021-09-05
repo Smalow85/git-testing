701d2f2de98a0ad286900ebe48e07095b7cf436d
Evgeny Kononov <evgeny_kononov@epam.com>
2021-09-06 00:46:35 +0300

    Master 2nd commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,8 @@
+import util.Utils;
+
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello, World");
+		String s = "Hello, World";
+		Utils.printString(s);
 	}
 }
---
+++ src/main/java/util/Utils.java
@@ -0,0 +1,7 @@
+package util;
+
+public class Utils {
+	public static void printString(String s){
+		System.out.println(s);
+	}
+}
