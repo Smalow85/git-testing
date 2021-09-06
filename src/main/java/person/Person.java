da8137754d9f4223abcaeb1eb6da0613c243500e
Evgeny Kononov <evgeny_kononov@epam.com>
2021-09-06 11:21:03 +0300

    change print output

--- src/main/java/person/Person.java
+++ src/main/java/person/Person.java
@@ -3,19 +3,10 @@ package person;
 public class Person {
 	private String name;
 	private int age;
-	private String petName;
 
 	public Person(String name, int age) {
 		this.name = name;
 		this.age = age;
-	}
-
-	public String getPetName() {
-		return petName;
-	}
-
-	public void setPetName(String petName) {
-		this.petName = petName;
 	}
 
 	public String getName() {
