035336b266f78e3909adcf5fd3111bdc5805bc0a
Evgeny Kononov <evgeny_kononov@epam.com>
2021-09-06 11:15:29 +0300

    change print output

--- src/main/java/person/Person.java
+++ src/main/java/person/Person.java
@@ -10,6 +10,14 @@ public class Person {
 		this.age = age;
 	}
 
+	public String getPetName() {
+		return petName;
+	}
+
+	public void setPetName(String petName) {
+		this.petName = petName;
+	}
+
 	public String getName() {
 		return name;
 	}
