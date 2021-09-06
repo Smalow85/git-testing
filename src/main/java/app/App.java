edadb28795b9dbccf8b9a337b6cb5ef4d770b3f5
Evgeny Kononov <evgeny_kononov@epam.com>
2021-09-06 11:13:18 +0300

    change print output

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -7,5 +7,6 @@ public class App {
 	public static void main(String[] args) {
 		Person person = new Person("Zhenya", 36);
 		Utils.printUpperCase("Person's age is " + person.getAge());
+		Utils.printString(person.getName());
 	}
 }
